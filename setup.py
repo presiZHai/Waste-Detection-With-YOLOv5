8c7603302b645d9495ac3423813109d947d36f2d
Olawale Ogundeji <abiodungndj@gmail.com>
2024-07-05 12:04:44 +0100

    notebook experiment added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'GarbageGauge',
+    version= '0.0.0',
+    author= 'Olawale Ogundeji',
+    author_email= 'abiodungndj@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
